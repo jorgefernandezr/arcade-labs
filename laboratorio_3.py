ce4b469880decd3018781bfbd6f3c889a7ddc531
jorgefernandezr <jorge.fernandezr@edu.uah.es>
2020-03-12 18:21:20 +0100

    Primer commit practica 5

--- laboratorio_3.py
+++ laboratorio_3.py
@@ -5,9 +5,11 @@ Este es un dibujo
 import arcade
 import random
 
+
 def cesped():
     # Cesped
     arcade.draw_lrtb_rectangle_filled(0, 800, 200, 0, arcade.color.GREEN)
+
 
 def casa():
     # Casa
@@ -38,7 +40,8 @@ def casa():
     arcade.draw_lrtb_rectangle_filled(200, 250, 250, 150, arcade.color.BROWN)
     arcade.draw_circle_filled(205, 200, 3, arcade.color.YELLOW)
 
-def persona(x,y):
+
+def persona(x, y):
     arcade.draw_lrtb_rectangle_filled(600 + x, 601 + x, 225 + y, 175 + y, arcade.color.BLACK)
     arcade.draw_rectangle_filled(611 + x, 215 + y, 25, 2, arcade.color.BLACK, 330)
     arcade.draw_rectangle_filled(589 + x, 215 + y, 25, 2, arcade.color.BLACK, 30)
@@ -46,38 +49,38 @@ def persona(x,y):
     arcade.draw_rectangle_filled(594 + x, 166 + y, 25, 2, arcade.color.BLACK, 60)
     arcade.draw_circle_filled(600 + x, 229 + y, 8, arcade.color.BLACK)
 
-def sol(x ,y):
+
+def sol(x, y):
     arcade.draw_circle_filled(-50 + x, 500 + y, 25, arcade.color.YELLOW)
+
 
 def luna(x, y):
     arcade.draw_circle_filled(-50 + x, 500 + y, 25, arcade.color.WHITE)
 
+
 def bucle_sol_luna():
     if dibujar.movimiento_luna == (850):
         dibujar.movimiento_sol = 0
-        sol(dibujar.movimiento_sol,0)
+        sol(dibujar.movimiento_sol, 0)
 
     elif dibujar.movimiento_sol == (850):
         dibujar.movimiento_luna = 0
         luna(dibujar.movimiento_luna, 0)
 
-    dibujar.movimiento_sol += 10
-    dibujar.movimiento_luna += 10
-    dibujar.movimiento_persona += -3
 
 def dia_mediodia_noche():
-
-    if(dibujar.movimiento_sol) == (000 or 800):
+    if dibujar.movimiento_sol == (000 or 800):
         arcade.set_background_color(arcade.color.ORANGE)
 
-    elif(dibujar.movimiento_sol) == (50):
+    elif dibujar.movimiento_sol == 50:
         arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
 
-    elif(dibujar.movimiento_luna) == (100):
+    elif dibujar.movimiento_luna == 100:
         arcade.set_background_color(arcade.color.BLACK)
 
-    elif(dibujar.movimiento_luna) == (800):
+    elif dibujar.movimiento_luna == 800:
         arcade.set_background_color(arcade.color.ORANGE)
+
 
 def dibujar(tiempo):
     arcade.start_render()
@@ -92,16 +95,21 @@ def dibujar(tiempo):
 
     dia_mediodia_noche()
 
-def main():
+    dibujar.movimiento_sol += 10
+    dibujar.movimiento_luna += 10
+    dibujar.movimiento_persona += -3
 
+
+def main():
     # Para abrir el programa
     arcade.open_window(800, 600, "Ejemplo")
     arcade.set_background_color(arcade.color.ORANGE)
-    arcade.schedule(dibujar, 1/60)
+    arcade.schedule(dibujar, 1 / 60)
 
     # Cierre
     arcade.finish_render()
     arcade.run()
+
 
 # Programa principal
 dibujar.movimiento_luna = -850
